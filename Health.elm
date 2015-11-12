401a97fe5af69a4536da9114aeb6983521ce9b28
Brian Hicks <brian@brianthicks.com>
2015-11-12 14:00:09 -0600

    styles: add chronos logo

---
+++ Health.elm
@@ -0,0 +1,18 @@
+module Health where
+
+-- MODEL
+
+type alias Health = { node : String
+                    , checkID : String
+                    , name : String
+                    , status : String
+                    , notes : String
+                    , output : String
+                    , serviceID : String
+                    , serviceName : String }
+
+-- UPDATE
+
+-- ACTIONS
+
+-- VIEW
